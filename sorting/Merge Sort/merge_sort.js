6fab7b74d36999bfdc33e4b507a59693cdc80de3
Shivang Mitra <shivangmitra8@gmail.com>
2021-06-23 17:42:04 +0530

    Added functionality to merge sort control buttons

--- sorting/Merge Sort/merge_sort.js
+++ sorting/Merge Sort/merge_sort.js
@@ -1,3 +1,12 @@
+//var to check wether to stop the visulaiser or not
+var to_exit = 0;
+
+//Tracking speed on slider
+var slider = document.getElementById("myRange")
+function changeSpeed(){
+	return(slider.value)
+}
+
 // Canvas variables
 var canvas, canvaswidth, canvasheight, ctrl;
 
@@ -14,15 +23,19 @@ var arr = [], itmd = [], visited = []
 var len_of_arr = 40;
 
 // Store random value in arr[]
-for (var i = 0; i < len_of_arr; i++) {
-	arr.push(Math.round(Math.random() * 250) )
-}
+function randomiseArray(){
+	for (var i = 0; i < len_of_arr; i++) {
+		arr.push(Math.round(Math.random() * 250) )
+	}
 
-// Initialize itmd and visited array with 0
-for (var i = 0; i < len_of_arr; i++) {
-	itmd.push(0)
-	visited.push(0)
+	// Initialize itmd and visited array with 0
+	for (var i = 0; i < len_of_arr; i++) {
+		itmd.push(0)
+		visited.push(0)
+	}
 }
+randomiseArray()
+
 
 // Merging of two sub array
 function mergeArray(start, end) {
@@ -66,8 +79,26 @@ function mergeArray(start, end) {
 	}
 }
 
+function initialDrawBars(){
+	ctrl.clearRect(0,0,2000,2000)
+	for(let i = 0; i < len_of_arr; i++){
+		// Changing styles of bars
+		ctrl.fillStyle = "#343434"
+		
+		
+		// Size of rectangle of bars
+		ctrl.fillRect(35 * i + 60, 300 - arr[i], 30, arr[i])
+
+        ctrl.font = "bold 14px Verdana"
+        ctrl.fillText(arr[i], 35 * i + 60, 300 - arr[i] - 2)
+	}
+}
+
 // Function for showing visualization effect
 function drawBars(start, end) {
+	if(to_exit === 1){
+		return
+	}
 
 	// Clear pervious unsorted bars
 	ctrl.clearRect(0, 0, 2000, 2000)
@@ -106,6 +137,9 @@ function timeout(ms) {
 
 // Merge Sorting
 const mergeSort = async (start, end) => {
+	if(to_exit === 1){
+		return
+	}
 	if (start < end) {
 		let mid = parseInt((start + end) >> 1)
 		await mergeSort(start, mid)
@@ -113,14 +147,17 @@ const mergeSort = async (start, end) => {
 		await mergeArray(start, end)
 		await drawBars(start, end)
 
-		// Waiting time is 800ms
-		await timeout(800)
+		// Waiting time
+		await timeout(changeSpeed())
 	}
 }
 
 // canvasElements function for storing
 // width and height in canvas variable
 function canvasElements() {
+	if(to_exit === 1){
+		return
+	}
 	canvas = document.getElementById("Canvas")
 	canvas.width = 1536
     canvas.height = 400
@@ -131,7 +168,53 @@ function canvasElements() {
 
 // Asynchronous MergeSort function
 const performer = async () => {
+	if(to_exit === 1){
+		return
+	}
 	await mergeSort(0, len_of_arr - 1)
 	await drawBars()
 }
-performer()
+
+// Drawing generated array and calling visualiser on click of start button
+initialDrawBars()
+var start = document.getElementById("start")
+start.addEventListener("click", () => {
+	performer(); 
+	start.disabled = true; 
+	stop.disabled = false;
+	start.classList.remove("ctrl-btn1"); 
+	start.classList.add("disable1")
+	stop.classList.remove("disable2"); 
+	stop.classList.add("ctrl-btn2")
+})
+
+// Stop button functionality
+var stop = document.getElementById("stop")
+stop.addEventListener("click", () => {
+	to_exit = 1; 
+	stop.disabled = true; 
+	stop.classList.remove("ctrl-btn2"); 
+	stop.classList.add("disable2")
+})
+
+// Reset button functionality
+var reset = document.getElementById("reset")
+reset.addEventListener("click", () => {
+	to_exit = 1
+	reset.classList.add("loading-rotate")
+	setTimeout(() => {
+		to_exit = 0; 
+		arr = []; 
+		itmd = []; 
+		visited = []; 
+		randomiseArray(); 
+		initialDrawBars();
+		start.disabled = false;
+		stop.disabled = true;
+		start.classList.remove("disable1"); 
+		start.classList.add("ctrl-btn1");
+		stop.classList.remove("ctrl-btn2"); 
+		stop.classList.add("disable2")
+		reset.classList.remove("loading-rotate")
+	}, 2000);
+})
